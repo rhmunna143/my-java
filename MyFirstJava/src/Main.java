7d12a877db87465ec25b595fe1d3eec7387e8118
rhmunna143 <rhmunna19@gmail.com>
2023-10-31 00:31:31 +0600

    first commit

---
+++ MyFirstJava/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 535;
+        String name = "Munna";
+        System.out.println("I am a React Developer learning JAVA now.");
+        System.out.println("My name is- ");
+        System.out.println(name);
+        System.out.println("I have: ");
+        System.out.println(x);
+        System.out.println("$ balance now.");
+    }
+    
+}
---
+++ my-firstjava/src/main/java/com/mycompany/my/firstjava/MyFirstjava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.my.firstjava;
+
+/**
+ *
+ * @author WALTON
+ */
+public class MyFirstjava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! to Munna");
+    }
+}
